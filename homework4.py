3eb3501d334721d1d126411111e5dd53d084f326
Radli1380 <ildarshakirow@mail.ru>
2024-05-16 20:21:40 +0300

    Add files via upload

---
+++ homework4.py
@@ -0,0 +1,8 @@
+immutable_var = (1,2,'a','b')
+print(immutable_var)
+immutable_var = [1,2,'a',True]
+print(immutable_var)
+immutable_list = (1,2,'a','b')
+print(immutable_list)
+immutable_list = [1,2,'a',True]
+print(immutable_list)
